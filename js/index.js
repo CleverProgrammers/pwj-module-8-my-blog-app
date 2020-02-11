27d317b9b57e93af5da42a5aa00050f9edfb5f9c
Nazariy995 <nazariy1995@gmail.com>
2020-02-11 00:06:58 -0500

    ADD: invidual post

--- js/index.js
+++ js/index.js
@@ -21,20 +21,23 @@ const buildPosts = (blogPosts) => {
     let blogPostsContent = "";
     for(blogPost of blogPosts){
         const postImage = API_BASE_URL + blogPost.post_image;
+        const postLink = `/post.html?id=${blogPost.id}`;
         const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
         blogPostsContent += `
-        <div class="post">
-            <div class="post-image" style="background-image: url(${postImage})"></div>
-            <div class="post-content">
-                <div class="post-date">${postDate}</div>
-                <div class="post-title">
-                    <h4>${blogPost.title}</h4>
+            <a class="post-link" href="${postLink}">
+                <div class="post">
+                    <div class="post-image" style="background-image: url(${postImage})"></div>
+                    <div class="post-content">
+                        <div class="post-date">${postDate}</div>
+                        <div class="post-title">
+                            <h4>${blogPost.title}</h4>
+                        </div>
+                        <div class="post-text">
+                            <p>${blogPost.content}</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="post-text">
-                    <p>${blogPost.content}</p>
-                </div>
-            </div>
-        </div>
+            </a>
         `
     }
     document.querySelector(".blog-posts").innerHTML = blogPostsContent;
